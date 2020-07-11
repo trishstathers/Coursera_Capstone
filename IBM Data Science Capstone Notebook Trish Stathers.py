2a912bb7ba3c7d2aae28f61c3082ef57bbf60d07
Trish Stathers <63172552+trishstathers@users.noreply.github.com>
2020-07-11 11:04:57 -0700

    Adds notebook

---
+++ IBM Data Science Capstone Notebook Trish Stathers.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## IBM Data Science Capstone Notebook 
+# Trish Stathers
+# 2020/07/11
+# 
+
+# In[1]:
+
+
+import pandas as pd 
+import numpy as np 
+print("Hello Capstone Project Course")
+
+
+# In[ ]:
+
+
+
+
